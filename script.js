f592b87462ac34804c23610e82f16690729370d0
mina <67394030+MinaSamirSaad@users.noreply.github.com>
2021-08-02 13:42:46 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,80 @@
+let searchBut = document.querySelector("#search-btn");
+let searchBar = document.querySelector(".search-bar-container");
+let formBtn = document.querySelector("#login-btn");
+let loginForm = document.querySelector(".login-form-container");
+let closeBtn = document.querySelector("#form-close");
+let menu = document.querySelector("#menu-bar");
+let navbar = document.querySelector(".navbar")
+let videoBtn = document.querySelectorAll(".vid-btn")
+
+window.onscroll= ()=>{
+    searchBut.classList.remove("fa-times");
+    searchBar.classList.remove("active")
+    menu.classList.remove("fa-times");
+    navbar.classList.remove("active");
+    loginForm.classList.remove("active");
+}
+
+searchBut.addEventListener("click",()=>{
+    searchBut.classList.toggle("fa-times");
+    searchBar.classList.toggle("active")
+})
+menu.addEventListener("click",()=>{
+    menu.classList.toggle("fa-times");
+    navbar.classList.toggle("active")
+})
+formBtn.addEventListener("click" ,()=>{
+    loginForm.classList.add("active");
+})
+closeBtn.addEventListener("click" ,()=>{
+    loginForm.classList.remove("active");
+})
+videoBtn.forEach(btn =>{
+    btn.addEventListener("click",()=>{
+        document.querySelector(".controls .active").classList.remove("active");
+        btn.classList.add("active");
+        let src = btn.getAttribute("data-src");
+        document.querySelector("#video-slider").src=src
+    })
+})
+var swiper = new Swiper(".review-slider", {
+  spaceBetween: 20,
+  loop:true,
+  autplay : {
+    delay:2500,
+    disableOnInteraction: false,
+  },
+  breakpoints:{
+    640:{
+        slidesPerView:1,
+    },
+    780:{
+        slidesPerView:2,
+    },
+    1024:{
+        slidesPerView:3,
+    },
+  },
+  });
+  var swiper = new Swiper(".brand-slider", {
+    spaceBetween: 20,
+    loop:true,
+    autplay : {
+      delay:2500,
+      disableOnInteraction: false,
+    },
+    breakpoints:{
+      450:{
+          slidesPerView:2,
+      },
+      768:{
+          slidesPerView:3,
+      },
+      991:{
+          slidesPerView:4,
+      },
+      1200:{
+        slidesPerView:5,
+    },
+    },
+    });
